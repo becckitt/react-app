bd675665a76efb4f15bb42cb6f435db2ff573ee6
Becca Barton <becca.barton@voxmedia.com>
2019-06-14 10:17:16 -0400

    add signup component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './Header';
+import Signup from './Signup';
 import '../css/styles.scss';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Signup></Signup>
     </div>
   );
 }
