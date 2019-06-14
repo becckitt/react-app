c4211915db376fe0202eff01849e7398f3f54cee
Becca Barton <becca.barton@voxmedia.com>
2019-06-14 17:54:23 -0400

    change double quotes to single quotes

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -5,22 +5,22 @@ class Signup extends React.Component {
     return (
       <form action='' className='signup-form'>
         <div className='full-name'>
-          <div className="first-name">
-            <label htmlFor="firstName">First name</label>
+          <div className='first-name'>
+            <label htmlFor='firstName'>First name</label>
             <input name='firstName' type='text' placeholder='Margaret'/>
           </div>
-          <div className="last-name">
-            <label htmlFor="lastName">Last name</label>
+          <div className='last-name'>
+            <label htmlFor='lastName'>Last name</label>
             <input name='lastName' type='text' placeholder='Hamilton'/>
           </div>
         </div>
         <p className='description'>The following information needs to be real for your payment to be processed.</p>
-        <div className="email-input">
-          <label htmlFor="email">Email</label>
+        <div className='email-input'>
+          <label htmlFor='email'>Email</label>
           <input name='email' type='text' placeholder='margaret@nasa.gov'/>
         </div>
-        <div className="payment-input">
-          <label htmlFor="payment">Saved payment</label>
+        <div className='payment-input'>
+          <label htmlFor='payment'>Saved payment</label>
           <input name='payment' type='text' placeholder='xxxx xxxx xxxx 4242' disabled/>
         </div>
         <button type='submit'>See who paid 99&#162;</button>
--- src/components/SuccessBanner.js
+++ src/components/SuccessBanner.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const SuccessBanner = () => (
-  <div className="success-banner">
+  <div className='success-banner'>
     You're in. Welcome to the club.
   </div>
 );
