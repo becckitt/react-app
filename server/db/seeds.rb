6a9a3ee23e588e606abde68e95e1e750ed5f7d7b
Becca Barton <becca.barton@voxmedia.com>
2019-06-16 13:32:47 -0400

    add some quick handling for members with 3 names

--- server/db/seeds.rb
+++ server/db/seeds.rb
@@ -33,6 +33,8 @@ members = [
 
 members.each do |member_name|
   names = member_name.split(" ")
-  email = "#{names[0]}.#{names[1]}@gmail.com"
-  User.create(first_name: names[0], last_name: names[1], email: email)
+  # Special handling in case they have a middle initial/name
+  last_name = names[1..-1]
+  email = "#{names[0]}.#{last_name.join(".")}@gmail.com"
+  User.create(first_name: names[0], last_name: last_name.join(" "), email: email)
 end
