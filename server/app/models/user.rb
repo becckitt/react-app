561c47665caa49c5ae0815c2026a22a7a41758b7
Becca Barton <becca.barton@voxmedia.com>
2019-06-16 01:08:35 -0400

    add test support

--- server/app/models/user.rb
+++ server/app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
-  validates :first_name, presence: true
-  validates :last_name,  presence: true
-  validates :email,      uniqueness: true, format: { :with => URI::MailTo::EMAIL_REGEXP }
+  validates_presence_of :first_name, :last_name
+  validates :email, uniqueness: true, format: { :with => URI::MailTo::EMAIL_REGEXP }
 end
