462fdf360f6eff031f45442099722fa8c2b79c15
Becca Barton <becca.barton@voxmedia.com>
2019-06-16 14:45:41 -0400

    add create user route to api

--- server/app/controllers/api/users_controller.rb
+++ server/app/controllers/api/users_controller.rb
@@ -3,4 +3,19 @@ class Api::UsersController < ApplicationController
     users = User.serialized_users
     render :json => users
   end
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: { status: 'SUCCESS', message: 'User saved', data: user }
+    else
+      render json: { status: 'ERROR', message: 'user not saved'}
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :payment)
+  end
 end
