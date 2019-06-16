0f21996e7974961d1972e047334e86eae0c02ee1
Becca Barton <becca.barton@voxmedia.com>
2019-06-16 00:38:46 -0400

    add user model

--- server/db/seeds.rb
+++ server/db/seeds.rb
@@ -1,7 +1,38 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+
+members = [
+  'Gladys West',
+  'Annie Easley',
+  'Ada Lovelace',
+  'Adele Mildred Koss',
+  'Margaret Hamilton',
+  'Grace Hopper',
+  'Anna Winlock',
+  'Annie Jump Cannon',
+  'Mary Clem',
+  'Clara Froelich',
+  'Mary K. Hawes',
+  'Kathryn Peddrew',
+  'Katherine Johnson',
+  'Thelma Estrin',
+  'Joyce Aylard',
+  'Joan Clarke',
+  'Margaret Rock',
+  'Louise Pearsall',
+  'Hedy Lamarr',
+  'Marlyn Meltzer',
+  'Kathleen Antonelli',
+  'Ruth Teitelbaum',
+  'Jean Bartik',
+  'Frances Spence',
+  'Barbara Paulson',
+  'Beatrice Worsley',
+  'Betty Holbertson',
+  'Edith Clarke'
+]
+
+members.each do |member_name|
+  names = member_name.split(" ")
+  email = "#{names[0]}.#{names[1]}@gmail.com"
+  User.create(first_name: names[0], last_name: names[1], email: email)
+end
