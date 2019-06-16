80a6bab05c7dee5e868751ac00a698108911cec3
Becca Barton <becca.barton@voxmedia.com>
2019-06-16 14:46:17 -0400

    add post request to submit form

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,23 +1,66 @@
 import React from 'react';
 
 class Signup extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      firstName: '',
+      lastName: '',
+      email: '',
+      payment: ''
+    }
+
+    this.createUser = this.createUser.bind(this);
+  }
+
+  createUser = event => {
+    event.preventDefault();
+    fetch('/api/user', {
+      method: 'post',
+      headers: {'Content-Type':'application/json'},
+      body: JSON.stringify({
+        first_name: this.state.firstName,
+        last_name: this.state.lastName,
+        email: this.state.email
+      })
+    });
+  }
   render() {
     return (
-      <form action='' className='signup-form'>
+      <form className='signup-form' onSubmit={this.createUser}>
         <div className='full-name'>
           <div className='first-name'>
             <label htmlFor='firstName' className='required'>First name</label>
-            <input required name='firstName' type='text' placeholder='Margaret'/>
+            <input required
+              name='firstName'
+              type='text'
+              placeholder='Margaret'
+              value={this.state.firstName}
+              onChange={e => this.setState({firstName: e.target.value})}
+            />
           </div>
           <div className='last-name'>
             <label htmlFor='lastName' className='required'>Last name</label>
-            <input required name='lastName' type='text' placeholder='Hamilton'/>
+            <input required
+              name='lastName'
+              type='text'
+              placeholder='Hamilton'
+              value={this.state.lastName}
+              onChange={e => this.setState({lastName: e.target.value})}
+            />
           </div>
         </div>
         <p className='description'>The following information needs to be real for your payment to be processed.</p>
         <div className='email-input'>
           <label htmlFor='email'>Email</label>
-          <input name='email' type='text' placeholder='margaret@nasa.gov'/>
+          <input
+            name='email'
+            type='text'
+            placeholder='margaret@nasa.gov'
+            value={this.state.email}
+            onChange={e => this.setState({email: e.target.value})}
+          />
         </div>
         <div className='payment-input'>
           <label htmlFor='payment'>Saved payment</label>
