f05d8710eddb131c440e90f887e3f2d9f7e6501d
Becca Barton <becca.barton@voxmedia.com>
2019-06-19 01:14:21 -0400

    generate code with securerandom

--- server/lib/referral_helper.rb
+++ server/lib/referral_helper.rb
@@ -1,24 +1,15 @@
+require 'digest'
+
 module ReferralHelper
   def generate_referral_code(id)
-    # would generate a better coupon code here w/ more time
-    # maybe after this could map each number to the alphabet
-    # and randomly capitalize so it looks like 'aHyjUki'
-    # could also just use a sha hash like github uses for commits
-    id + hashing_key
-  end
-
-  def decode_referral_code(code)
-    id - hashing_key
+    # considered using MD5 but it looks long for
+    # a referral code. Went with SecureRandom instead
+    # Digest::MD5.hexdigest id.to_s
+    SecureRandom.hex(3)
   end
 
   def referral_link
     # would replace this with a real link
     "http://localhost:3001/x/#{referral_code}"
-  end
-
-  private
-
-  def hashing_key
-    12345
   end
 end
