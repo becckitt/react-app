68b58ae24102ca9fa5d4d90c9633f938cab78b99
Becca Barton <becca.barton@voxmedia.com>
2019-06-18 22:31:04 -0400

    add success banner to members page

--- client/src/components/Members.js
+++ client/src/components/Members.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Header from './Header';
+import SuccessBanner from './SuccessBanner';
 import MembersList from './MembersList';
 import ReferralFooter from './ReferralFooter';
 
 class Members extends React.Component {
   render() {
     return (
-      <div className="App">
+      <div className='App'>
         <Header></Header>
+        <SuccessBanner></SuccessBanner>
         <MembersList></MembersList>
         <ReferralFooter></ReferralFooter>
       </div>
